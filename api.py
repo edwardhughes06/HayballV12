b146742fd1fca63c751367ab12bab8bde9977447
Edward Hughes <contact@edhughes.co.uk>
2024-11-20 00:26:04 +0000

    Style update. pass name and price data

--- api.py
+++ api.py
@@ -23,70 +23,6 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 api = Api(app)
 
-class UserModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"User(name = {self.name}, email = {self.email})"
-
-user_args = reqparse.RequestParser()
-user_args.add_argument('name', type=str, required=True, help="Name cannot be blank")
-user_args.add_argument('email', type=str, required=True, help="Email cannot be blank")
-
-userFields = {
-    'id':fields.Integer,
-    'name':fields.String,
-    'email':fields.String,
-}
-
-class Users(Resource):
-    @marshal_with(userFields)
-    def get(self):
-        users = UserModel.query.all()
-        return users
-    @marshal_with(userFields)
-    def post(self):
-        args = user_args.parse_args()
-        user = UserModel(name=args["name"],email=args["email"])
-        db.session.add(user)
-        db.session.commit()
-        users = UserModel.query.all()
-        return users, 201
-
-class User(Resource):
-    @marshal_with(userFields)
-    def get(self, id):
-        user = UserModel.query.filter_by(id=id).first()
-        if not user:
-            abort(404, "User not found")
-        return user    
-    
-    @marshal_with(userFields)
-    def patch(self, id):
-        args = user_args.parse_args()
-        user = UserModel.query.filter_by(id=id).first()
-        if not user:
-            abort(404, "User not found")
-        user.name = args["name"]
-        user.email = args["email"]
-        db.session.commit()
-        return user 
-       
-    @marshal_with(userFields)
-    def delete(self, id):
-        user = UserModel.query.filter_by(id=id).first()
-        if not user:
-            abort(404, "User not found")
-        db.session.delete(user)
-        db.session.commit()
-        user = UserModel.query.all()
-        return user, 200
-    
-
-api.add_resource(Users, '/api/users/')
-api.add_resource(User, '/api/users/<int:id>')
 
 @app.route('/finance_products', methods=['GET', 'POST'])
 def get_finance_products():
@@ -101,10 +37,21 @@ def get_finance_products():
 
     session["products-list"] = products
     session["id-list"] = id_list
+    product_value = 0
+    bike_name = request.args.get('bike')
+    if bike_name:
+        session["bike_name"] = bike_name
+
+    price = request.args.get('price')
+    if price:
+        cash_price = price
+        session["cash-price"] = cash_price
+
 
     if request.method == 'POST':
         product_id = int(request.form.get("product-dropdown"))
-        cash_price = request.form.get("cash-price-input")
+        if not cash_price:
+            cash_price = request.form.get("cash-price-input")
         deposit_amount = request.form.get("deposit-input")
         product_guid = product_details[id_list.index(product_id)][2]
 
@@ -127,14 +74,11 @@ def get_finance_products():
         payload["Order"]["Deposit"] = deposit_amount
         payload["Order"]["ProductGuid"] = product_guid
         payload["Order"]["ProductId"] = str(product_id)
-        payload["Order"]["SalesReference"] = str(random.randint(0,999999))
+        payload["Order"]["SalesReference"] = bike+str(random.randint(0,999999))
 
         payload["Retailer"]["AuthenticationKey"] = os.getenv("AuthenticationKey")
         payload["Retailer"]["RetailerGuid"] = os.getenv("RetailerGuid")
         payload["Retailer"]["RetailerId"] = os.getenv("RetailerId")
-
-        print("PAYLOAD",payload)
-
 
         reqUrl = "https://apply.v12finance.com/latest/retailerapi/SubmitApplication"
 
@@ -167,5 +111,5 @@ def index():
     return '<h1> REST API</h1>'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=False)
+    app.run(host='127.0.0.1', port=800, debug=True)
  
